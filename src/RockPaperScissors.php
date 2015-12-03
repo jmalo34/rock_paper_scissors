80c3a7012f560a839c50b6b1498c83e1f9c823bd
Jenna Maloney <jmalo@uw.edu>
2015-12-02 22:21:26 -0800

    refactor src code

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -3,20 +3,17 @@
     {
         function playGame($p1, $p2)
         {
-            if ($p1 == $p2)
+            if ((($p1 == 'Rock') && ($p2 == 'Scissors')) || (($p1 == 'Paper') && ($p2 == 'Rock')) || (($p1 == 'Scissors') && ($p2 == 'Paper')))
+            {
+                return 'Player 1';
+            }
+            elseif ($p1 == $p2)
             {
                 return 'Draw. Play again.';
             }
             else
             {
-                if ((($p1 == 'Rock') && ($p2 == 'Scissors')) || (($p1 == 'Paper') && ($p2 == 'Rock')) || (($p1 == 'Scissors') && ($p2 == 'Paper')))
-                {
-                    return 'Player 1';
-                }
-                else
-                {
-                    return 'Player 2';
-                }
+                return 'Player 2';
             }
         }
     }
