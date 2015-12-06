a5fdc06771b17db1927b069f8ca38634777e570c
Jenna Maloney <jmalo@uw.edu>
2015-12-06 14:18:01 -0800

    alter game, so you can play alone, against the computer

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,11 +1,14 @@
 <?php
     class RockPaperScissors
     {
-        function playGame($p1, $p2)
+        function playGame($p1)
         {
-            if ((($p1 == 'Rock') && ($p2 == 'Scissors')) || (($p1 == 'Paper') && ($p2 == 'Rock')) || (($p1 == 'Scissors') && ($p2 == 'Paper')))
+            $computer_throw = array('Rock', 'Paper', 'Scissors');
+            $p2 = array_rand($computer_throw);
+
+            if ((($p1 == 'Rock') && ($p2 == 3)) || (($p1 == 'Paper') && ($p2 == 1)) || (($p1 == 'Scissors') && ($p2 == 2)))
             {
-                return 'Player 1';
+                return 'You win!';
             }
             elseif ($p1 == $p2)
             {
@@ -13,7 +16,7 @@
             }
             else
             {
-                return 'Player 2';
+                return 'You lose';
             }
         }
     }
