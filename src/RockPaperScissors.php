3b6459461108a666a54de8a659746a524e2dce47
Jenna Maloney <jmalo@uw.edu>
2015-12-07 12:45:06 -0800

    fix function so comparison operators are used to evaluate the value of , rather than arithmetic operators

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -5,10 +5,23 @@
         {
             $computer_throw = array('Rock', 'Paper', 'Scissors');
             $p2 = array_rand($computer_throw);
-
-            if ((($p1 == 'Rock') && ($p2 == 3)) || (($p1 == 'Paper') && ($p2 == 1)) || (($p1 == 'Scissors') && ($p2 == 2)))
+            if ($p2 == 1)
             {
-                return 'You win!';
+                $p2 = 'Rock';
+            }
+            elseif ($p2 == 2)
+            {
+                $p2 = 'Paper';
+            }
+            else
+            {
+                $p2 = 'Scissors';
+            }
+
+            if ((($p1 == 'Rock') && ($p2 == 'Scissors')) || (($p1 == 'Paper') && ($p2 == 'Rock')) || (($p1 == 'Scissors') && ($p2 == 'Paper')))
+            {
+                $statement = $p1 . " beats " . $p2 . ". You win!";
+                return $statement;
             }
             elseif ($p1 == $p2)
             {
@@ -16,7 +29,8 @@
             }
             else
             {
-                return 'You lose';
+                $statement = "The computer threw " . $p2 . ". You lose.";
+                return $statement;
             }
         }
     }
